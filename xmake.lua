4e3e29fffbaf9f3bfd3f7dc625578772e080992f
Romain Chardiny <romain.chardiny@gmail.com>
2022-04-07 14:03:35 +0200

    things

--- xmake.lua
+++ xmake.lua
@@ -5,15 +5,34 @@ set_rules("mode.debug", "mode.release", "mode.valgrind")
 set_allowedmodes("debug", "release", "valgrind")
 set_defaultmode("debug")
 
-add_languages("cxx17")
-
 add_requires("cxxopts")
 
+add_includedirs("include")
+
+add_languages("cxx17")
 set_warnings("allextra")
 
 target("word-path-game")
+    set_group("word-path-game")
     set_kind("binary")
     add_files("src/*.cpp")
-    add_includedirs("include")
     add_headerfiles("include/*.hpp")
     add_packages("cxxopts")
+
+option("tests")
+  set_default(false)
+  set_showmenu(true)
+  set_description("Build unit tests")
+option_end()
+
+if has_config("tests") then
+    add_requires("catch2")
+
+    target("word-path-game-tests")
+        add_deps("word-path-game")
+        add_files("tests/*.cpp")
+        add_packages("catch2")
+        set_group("Tests")
+        set_kind("binary")
+        add_headerfiles("include/*.hpp")
+end
