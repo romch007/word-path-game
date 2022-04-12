008dd1c63b08818e82aaa4816d229b455e7e7abc
Romain Chardiny <romain.chardiny@gmail.com>
2022-04-12 14:15:16 +0200

    docs

--- include/utils.hpp
+++ include/utils.hpp
@@ -6,9 +6,24 @@
 
 namespace wordpath {
 
+  /**
+   * Check if a given word is in a given word list
+   */
   bool is_in(const word_list& words, const std::string& target);
+
+  /**
+   * Get a string representation of a rounded float
+   */
   std::string round_to_str(float number, unsigned int precision);
+
+  /**
+   * Lowercase whole string
+   */
   void lowercase(std::string& str);
+
+  /**
+   * Check if given words are all in a given dict
+   */
   bool check_for_words(const word_list& words, const dict& target_word);
 
 }
--- include/words.hpp
+++ include/words.hpp
@@ -8,15 +8,37 @@
 
 namespace wordpath {
 
+  /**
+   * A list of words
+   */
   using word_list = std::vector<std::string>;
+
+  /**
+   * Graph representation of linked words
+   */
   using dict = std::unordered_map<std::string, word_list>;
 
+  /**
+   * Compute neighbours of a given word
+   */
   void compute_neighbours(const std::string& source, const word_list& possible_words, std::ofstream& output, std::atomic_int& progess, int word_count);
+
+  /**
+   * Generate dictionnary file based on raw file
+   */
   void generate_dict(std::ifstream& file, std::ofstream& output);
+
+  /**
+   * Find path between two words given a dictionnary
+   */
   std::unique_ptr<word_list> find_path(const std::string& source,
                                        const std::string& target,
                                        std::unique_ptr<dict> words);
   void print_final_list(std::unique_ptr<word_list> words);
+
+  /**
+   * Parse dictionnary from file
+   */
   std::unique_ptr<dict> parse_dict_from_file(std::ifstream& dict_file);
 
 }
